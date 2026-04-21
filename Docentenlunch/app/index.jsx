b433317ede91108f0f36cbf0c0a9a694c362c29c
9024454 <9024454@student.zadkine.nl>
2026-04-21 15:14:23 +0200

    Update

---
+++ Docentenlunch/app/index.jsx
@@ -0,0 +1,21 @@
+import {Text, View } from 'react-native'
+export default function index() {
+  return (
+    <View>
+      <Text style={styles.text}>index</Text>
+    </View>
+  )
+}
+
+
+
+
+const styles = ({
+text: { 
+     color: '#671919',
+  fontSize: 20,
+  fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 20,
+},
+})
